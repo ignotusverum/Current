f5633de6449269941a64a2005ca1e8f7ba61bc8d
Vlad Z <vladislavz1991@gmail.com>
2020-01-30 18:19:24 -0500

    initial commit

---
+++ Fast Foodz/CFoundation/CFoundation.h
@@ -0,0 +1,18 @@
+//
+//  CFoundation.h
+//  CFoundation
+//
+//  Created by Vlad Z. on 1/30/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CFoundation.
+FOUNDATION_EXPORT double CFoundationVersionNumber;
+
+//! Project version string for CFoundation.
+FOUNDATION_EXPORT const unsigned char CFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CFoundation/PublicHeader.h>
+
+
